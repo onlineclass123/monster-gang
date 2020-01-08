db82143a53949176122df4442d9597ad1e51c87b
Chinedu <chinedu@crenettechlabs.com>
2020-01-08 12:22:59 +0100

    Updates

---
+++ precache-manifest.4c7c2d1843b66fd1ff6ea01efd37d896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da251b4850032868e32a875c248266c9",
+    "url": "/monster-gang/index.html"
+  },
+  {
+    "revision": "479bf2dbe01897eb41d8",
+    "url": "/monster-gang/static/css/main.772c36b3.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monster-gang/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-gang/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "479bf2dbe01897eb41d8",
+    "url": "/monster-gang/static/js/main.76719dd4.chunk.js"
+  },
+  {
+    "revision": "8025bbe0251cf1a998b3",
+    "url": "/monster-gang/static/js/runtime-main.8ef22895.js"
+  }
+]);
